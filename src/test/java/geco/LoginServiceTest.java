a78956b288d4ddd714cf236de598f12a117ee4cc
traoremoussa <moussa.traoreinfo@gmail.com>
2018-11-20 16:52:03 +0100

    #1 test-login-service

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -1,5 +1,29 @@
+package geco;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LoginServiceTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void tesLoginExists() {
+    }
+
+    @Test
+    public void testAddLogin() {
+    }
+
+    @Test
+    public void testFindAllLoginsStartingWith() {
+    }
+
+    @Test
+    public void testFindAllLogins() {
+    }
 }
